879d9dfe7f518c50b24bbd0ab67fc443d9deb688
Majid Kazemi <majid00kazemi@gmail.com>
2023-01-13 14:24:54 +0330

    added carousel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,39 @@
 const API_KEY = "api_key=04a874772b13520f53772f609285a97e";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const IMG_URL = "https://image.tmdb.org/t/p/original";
 
-const carouselItems = document.querySelectorAll(".carousel-item > img");
-
-carouselItems.forEach((item) => {
-  console.log(item);
-});
+const carouselContainer = document.querySelector(".carousel-inner");
 
 getMovies(API_URL);
 
 function getMovies(url) {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => {});
+    .then((data) => {
+      showMovies(data.results);
+      console.log(data.results);
+    });
+}
+
+function showMovies(data) {
+  data.forEach((movei, index) => {
+    const { title, poster_path } = movei;
+    const carousel = document.createElement("div");
+    const carouselImg = document.createElement("img");
+    if (index == 0) {
+      carousel.classList.add("active");
+    }
+    carousel.classList.add("carousel-item");
+    carouselImg.classList.add("d-block", "w-100");
+    carousel.setAttribute("data-bs-interval", 2000);
+
+    carouselImg.src = `${IMG_URL + poster_path}`;
+    carousel.appendChild(carouselImg);
+    carouselContainer.appendChild(carousel);
+    if (index === 10) {
+      return;
+    } else {
+    }
+  });
 }
