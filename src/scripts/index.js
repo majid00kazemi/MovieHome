5223036ccb608160ed9ca4015e2d942cc400a8b4
Majid Kazemi <majid00kazemi@gmail.com>
2023-01-14 00:01:10 +0330

    back to top option

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -169,3 +169,27 @@ function showLatestSeries(data) {
     }
   });
 }
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.visibility = "visible";
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.visibility = "hidden";
+    mybutton.style.visibility = "0";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
