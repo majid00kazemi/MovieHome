104f0b89b5c3313031838dd1534a66fdb11c075b
Majid Kazemi <majid00kazemi@gmail.com>
2023-01-18 14:17:18 +0330

    added base structure

---
+++ src/scripts/global.js
@@ -0,0 +1,22 @@
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.visibility = "visible";
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.visibility = "hidden";
+    mybutton.style.visibility = "0";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
